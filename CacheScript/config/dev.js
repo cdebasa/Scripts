cabc73ed2aa56893807a9c6b69f23da7c0637ddb
cdebasa <cdebasa@teckpert.com>
2020-01-21 18:12:50 -0500

    Broad
    Broad Changes

---
+++ CacheScript/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '340737975582-vggc601mtnev4f6ri5emjmdsin71lda1.apps.googleusercontent.com',
+  googleClientSecret: 'WtgJPuhxVMW_YKcbAAeCQsMe',
+  mongoURI: 'mongodb+srv://admin1234:admin1234@cluster0-1y4am.mongodb.net/test?retryWrites=true&w=majority  ',
+  cookieKey: '123123123'
+};
